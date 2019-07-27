7991e94ed24e65dab830b0d358e3772f3fa33f33
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2019-07-27 18:20:18 -0500

    Program to factor a number
    using java to find factors of a number

---
+++ MainApp.java
@@ -0,0 +1,59 @@
+package App;
+
+import java.util.Scanner;
+
+public class MainApp {
+	
+//	static aBeginning bg	 = new aBeginning();
+//	static projectGutenberg pg = new projectGutenberg();
+//	static CommentingCode cc = new CommentingCode();
+//	static AllTheMaths atm = new AllTheMaths();	
+//	static InABucket iab = new InABucket();
+//	static OrderOfThings oot = new OrderOfThings();
+	
+	public static void main(String[] args) {
+		
+		//declare the number to be factored
+		int fact;
+		int i;
+		
+		//collect the number to be factored
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter a number:");
+		fact = sc.nextInt();
+		
+		//Write a program to find the factors a number
+		System.out.println("Factors of "+fact+" are: ");
+		for (i = 1; i <= fact; i++) {
+			if(fact % i == 0) {
+				System.out.print(i+" ");
+				}
+		}
+		
+
+		
+	/*	bg.simpleMethod();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		pg.gutenberg(args);
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		cc.commenting();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		cc.commenting();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		atm.math();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+	
+		iab.inABucket();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		oot.order();
+		System.out.println("-----------------------------------------------------------------------------");
+		*/
+	}
+
+}
